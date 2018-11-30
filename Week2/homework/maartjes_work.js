6c940f2514a8c54b69d85e0495e3d895ad9e2bed
Rohan Nicholls <rohan.nicholls@gmail.com>
2018-11-30 16:59:13 +0100

    week 2 homework with bonuses

--- Week2/homework/maartjes_work.js
+++ Week2/homework/maartjes_work.js
@@ -44,4 +44,28 @@ const tuesday = [
 
 const tasks = monday.concat(tuesday);
 
+const debugging = true;
+const logging = (...message) => debugging && console.log(...message);
 // Add your code here
+const hoursWorked = tasks.map(item => item.duration / 60);
+logging(hoursWorked);
+const validHours = hoursWorked.filter(h => h > 2);
+logging('VALID HOURS', validHours);
+const maartjesRate = 100;
+const charged = validHours.map(n => n * maartjesRate);
+logging('CHARGED', charged);
+const sum = (a, b) => a + b;
+const total = charged.reduce(sum, 0);
+
+const reduceAllTheThings = () =>
+  tasks.reduce((total, item) => (item.duration > 120 ? total + (item.duration / 60) * maartjesRate : total), 0);
+
+console.log('REDUCE ALL THE THINGS', reduceAllTheThings(tasks).toLocaleString('en-EN', { style: 'currency', currency: 'EUR' }));
+
+logging(
+  'TOTAL',
+  total.toLocaleString('en-EN', {
+    style: 'currency',
+    currency: 'EUR'
+  })
+);
--- Week2/homework/map_filter.js
+++ Week2/homework/map_filter.js
@@ -3,3 +3,11 @@
 const numbers = [1, 2, 3, 4];
 
 // Add your code here
+// filter & map
+
+const doubleOdds = numbers.filter(n => n % 2 !== 0).map(x => x * 2);
+console.log(doubleOdds);
+
+// reduce version (only one pass)
+const redDouble = numbers.reduce((acc, n) => (n % 2 !== 0 ? [...acc, n * 2] : acc), []);
+console.log(redDouble);
